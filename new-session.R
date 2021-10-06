d764fd7c840c3ac4c3acce5c7e3764e131625871
anoopktiwari <80899122+anoopktiwari@users.noreply.github.com>
2021-10-06 11:11:53 +0530

    Update new-session.R

--- new-session.R
+++ new-session.R
@@ -1,7 +1,16 @@
-#################
-#################
+#################################
+#################################
 
 
 
 # taking a quick look of columns of df
 glimpse(df)
+
+# to remove perticular values from data frame
+output_name <- df[apply(df, 1, function(row) all(row !=1)), ]
+                        
+# to write df as required format
+write.table(genes_counts1, file = "15k_genecount.gct", row.names=T, sep="\t", quote=F)
+                        
+                        
+                        
