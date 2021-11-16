edd8ed9f07c16ab1e9b8005f2efc85362309fd82
anoopktiwari <80899122+anoopktiwari@users.noreply.github.com>
2021-11-16 12:38:42 +0530

    Update new-session.R

--- new-session.R
+++ new-session.R
@@ -21,6 +21,21 @@ add_column(cleared_tpm2, add_column="Dummy") -> cleared_tpm2
                         
 # relocate columns by their names
 cleared_tpm2 %>% relocate(add_column, .after = Name) -> cleared_tpm2
+
+# calculation of median, iqr and mad
+cleared.fpkm.MIM <- cleared_fpkm %>% rowwise() %>% mutate(data.median = median(c_across(3:151)),
+                                                                            data.iqr = IQR(c_across(3:151)),
+                                                                            data.mad = mad(c_across(3:151)),
+                                                                        data.mean = mean(c_across(3:151))
+                                                                            )
+# log2 calculation
+cleared_fpkm[,3:151] <- log2(cleared_fpkm[,3:151])
+
                         
+# correlation scatter plot
+ggscatter(cleared.fpkm.MIM, x = "data.median", y = "data.iqr",
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Median", ylab = "IQR")                        
                         
                         
